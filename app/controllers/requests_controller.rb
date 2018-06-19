265e75ee1d24dca8249a1703a11bb4ee6d337cda
Doug Bennett <dben89x@gmail.com>
2018-06-19 00:08:55 -0700

    idk

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,6 @@
 class RequestsController < ApplicationController
   skip_before_action :verify_authenticity_token
-  skip_before_action :authenticate_user!
+  skip_before_action :authenticate_user!, raise: false
   skip_load_and_authorize_resource
 
   def create
